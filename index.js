c813e7b04dc3daf826d0a896dd6c37fe82d87a1d
itsme-harsh <111678608+itsme-harsh@users.noreply.github.com>
2025-04-18 11:46:58 +0530

    -- intial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  socket.on('join', (room) => {
+    socket.join(room);
+    socket.to(room).emit('peer-connected');
+  });
+
+  socket.on('signal', ({ room, data }) => {
+    socket.to(room).emit('signal', data);
+  });
+});
+
+httpServer.listen(3000, () => {
+  console.log('🚀 Server running at http://localhost:3000');
+});
